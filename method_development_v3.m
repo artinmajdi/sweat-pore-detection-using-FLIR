1a137e5fa0baf92934ed72335879fce030b5c21c
artinmajdi <msm2024@gmail.com>
2019-11-06 20:33:52 -0700

    version 2.0

--- method_development_v3.m
+++ method_development_v3.m
@@ -1,38 +1,3 @@
-%%  extras for presentation
-%     clc
-%     clear
-%     h = [-1,-1,-1;-1,8,-1;-1,-1,-1];
-
-%     L = 7;
-%     h1 = -1.8*ones(L,L);
-%     h1(3:5,3:5) = 8;
-%     
-%     h2 = ones(L,L)/(L^2);    
-%     h3 = conv2(h1,h2);    
-
-    L = 3;
-    h1 = -1*ones(L,L);
-    h1(2,2) = 8;
-    
-    h2 = ones(L,L)/(L^2);    
-    h3 = conv2(h1,h2);    
-
-    [X,Y] = meshgrid(1:2*L-1 , 1:2*L-1);
-    surf(fittedmodel(X,Y),h3)
-    
-%     L2 = 50;
-%     x1 = ceil( (L2-(2*L-1))/2 );
-%     H = zeros(L2,L2);
-%     H(x1:x1+2*L-2,x1:x1+2*L-2) = h3;
-% %     [X,Y] = meshgrid(1:L2 , 1:L2);
-% %     surf(fittedmodel(X,Y),H)
-% 
-%     [X,Y] = meshgrid(1:2*L-1 , 1:2*L-1);
-%     surf(fittedmodel(X,Y),h3)
-
-%%
-% app_designer_address: 'C:\Users\artin\AppData\Roaming\MathWorks\MATLAB Add-Ons\Collections\App Designer - Image Processing'
-
 clc
 clear
 
@@ -42,48 +7,84 @@ index = 160;
 imO = read_image(index, dataset_dir, true);
 [im, Background] = removeBackground(imO);
 
-% myshow('joine','imtool', imO , im , Background)
 %% enhancing the dots
 
-% [im_filtered, candidates1] = apply_filter(im,0.4);
-[msk, im1 , im4] = apply_filter2(im, Background, "same");
+% [im_filtered, candidates1] = apply_filter_V1(im,0.4);
+[msk, im1 , im4] = apply_filter_V2(im, Background, "same");
 
+%% watershed segmentation
 % im5 = watershed(im4,4);
 % candidates1_watershed = keeping_the_watershed_area(watershed_output, candidates1);
-%% write the video
+
+%% write as a video
 L = 5950;
 save_video(dataset_dir, L)
 
-%%
-function [msk, im1 , im4] = apply_filter2(im, Background, shape)
+%% functions
+function [im1d, im1d_binarize] = apply_filter_V1(im1_woBg,sensitivity)
+    % local differentiation
+    h = [-1,-1,-1;-1,8,-1;-1,-1,-1];
+    im1c = filter2(h,im1_woBg,"valid");
+    im1c = func_normalize(im1c, 1);
+    
+    % local average
+    h = ones(3,3)/4;
+    im1d = filter2(h,im1c,"valid");
+    im1d = func_normalize(im1d, 1);
+    
+    im1d_binarize = ~imbinarize(im1d,'adaptive','ForegroundPolarity','dark','Sensitivity',sensitivity);
+end
+
+function [msk, im1 , im4] = apply_filter_V2(im, Background, shape)
+
+
+    % imA = rescale(imbilatfilt(im,0.5));
+    % imB = rescale(locallapfilt(im,0.2,0.2));
+    % imC = rescale(imdiffusefilt(im));  % ,Name,Value 
+    % imD = rescale(imnlmfilt(im , 'ComparisonWindowSize' , 3 , 'SearchWindowSize' , 11));  % ,Name,Value 
+    % imE = rescale(filter2( fspecial('average',3) ,im,"same"));
+    % myshow('joined','imshow', imE , imEc , im2c)
+
+    % ax(1) = subplot(231); imshow(im)  ; title('Original Image', 'FontSize',18)
+    % ax(2) = subplot(232); imshow(imA) ; title('Bilateral Fitler', 'FontSize',18) 
+    % ax(3) = subplot(233); imshow(imE) ; title('3x3 Average Filter', 'FontSize',18)
+    % ax(4) = subplot(234); imshow(imC) ; title('Anisotropic Diffusion Filter', 'FontSize',18)
+    % ax(5) = subplot(235); imshow(imD) ; title('Non-Local Means Filter', 'FontSize',18)
+    % ax(6) = subplot(236); imshow(imB) ; title('Local Laplacian Filter', 'FontSize',18)
+    % linkaxes(ax)
+
+
     % rescale(entropyfilt(im));
     % localcontrast(im, 0.2, 0.2);
 
     im1 = rescale(locallapfilt(im,0.2,0.2));
     im2 = rescale(filter2( fspecial('average',3) ,im1,shape));
+    % im2 = rescale(imnlmfilt(im1));
 
     h = [-1,-1,-1;-1,8,-1;-1,-1,-1];
     im2b = rescale(filter2(h,im2,shape));
-
+    % myshow('joined','imshow', im , im2 , im2b)  
+    
     im3 = rescale(imhmax(im2b,0.3));
-    im4 = rescale(filter2( fspecial('average',3) ,im3,shape));   
+    im4 = rescale(filter2( fspecial('average',3) ,im3,shape));       
     
     mskO = ~imbinarize(im4,'adaptive','ForegroundPolarity','dark','Sensitivity',0.2);
     msk2 = bwareafilt(mskO , [1, 15] );    
     msk = msk2;
     msk(Background == 1) = 0; 
     
-%     ax(1) = subplot(131); imshow(im)  ; title('Original Image', 'FontSize',18)% , hold on , visboundaries(msk)
-%     ax(2) = subplot(132); imshow(im4) ; title('Fitlered Image', 'FontSize',18) % , hold on , visboundaries(msk)
-%     ax(3) = subplot(133); imshow(im4) ; title('Fitlered Image With Detected pores', 'FontSize',18), hold on , visboundaries(msk)
-% %     ax(3) = subplot(143); imshow(mskO); title('Binary Mask', 'FontSize',18)
-% %     ax(4) = subplot(144); imshow(msk); title('Removing Background & Big Objects', 'fontsize',18)
+    
+%     ax(1) = subplot(141); imshow(im)  ; title('Original Image', 'FontSize',14)
+%     ax(2) = subplot(142); imshow(im1) ; title('Local Laplacian Filter', 'FontSize',14) 
+%     ax(3) = subplot(143); imshow(4*im2b) ; title('Manual Filter: Remove Background Texture', 'FontSize',14)
+%     ax(4) = subplot(144); imshow(4*im4) ; title('H-Maxima -> Average Filter', 'FontSize',14)
+%     linkaxes(ax) 
+    
+%     ax(1) = subplot(2,3,[1,2,4,5]); imshow(im(40:end,40:end))  ; title('Original Image', 'FontSize',18)% , hold on , visboundaries(msk)
+%     ax(2) = subplot(233); imshow(im4(40:end,40:end)) ; title('Fitlered Image', 'FontSize',18) % , hold on , visboundaries(msk)
+%     ax(3) = subplot(236); imshow(im(40:end,40:end)) ; title('Fitlered Image With Detected pores', 'FontSize',18), hold on , visboundaries(msk(40:end,40:end))
 %     linkaxes(ax)
     
-%     figure,
-%     ax(1) = subplot(121); imshow(mskO(330:370,160:200));title('Binary Mask', 'FontSize',18)
-%     ax(2) = subplot(122); imshow(msk2(330:370,160:200)); title('Removing Background & Big Objects', 'fontsize',18)
-%     linkaxes(ax)
 end
 
 function myshow(separate_joined,mode,varargin)
@@ -91,7 +92,7 @@ function myshow(separate_joined,mode,varargin)
         
         if strcmp(separate_joined,'joined')
             if strcmp(mode,'imshow')
-                ax(x) = subplot(1,length(varargin),x); imshow(varargin{x},[]);
+                ax(x) = subplot(1,length(varargin),x); imshow(varargin{x});
             elseif strcmp(mode,'imtool')
                 ax(x) = subplot(1,length(varargin),x); imtool(varargin{x},[]);
             end
@@ -155,20 +156,6 @@ function im = func_normalize(im,type)
     
 end
 
-function [im1d, im1d_binarize] = apply_filter(im1_woBg,sensitivity)
-    % local differentiation
-    h = [-1,-1,-1;-1,8,-1;-1,-1,-1];
-    im1c = filter2(h,im1_woBg,"valid");
-    im1c = func_normalize(im1c, 1);
-    
-    % local average
-    h = ones(3,3)/4;
-    im1d = filter2(h,im1c,"valid");
-    im1d = func_normalize(im1d, 1);
-    
-    im1d_binarize = ~imbinarize(im1d,'adaptive','ForegroundPolarity','dark','Sensitivity',sensitivity);
-end
-
 function mask = keeping_the_watershed_area(watershed_output, binary_mask)
     mskk = watershed_output;
     mskk(binary_mask == 0) = 0;
@@ -201,6 +188,7 @@ function save_video(dataset_dir, L)
         msk = post_process(im , im1 , prediction);
 
         frame = im2frame(msk);
+%         a = vision.PointTracker(frame);
         writeVideo(writerObj, frame);
     end
     close(writerObj);
@@ -210,3 +198,39 @@ function save_video(dataset_dir, L)
         msk = uint8(cat(3,merged,merged,merged));     
     end
 end
+
+
+% %%  extras for presentation
+% %     clc
+% %     clear
+% %     h = [-1,-1,-1;-1,8,-1;-1,-1,-1];
+% 
+% %     L = 7;
+% %     h1 = -1.8*ones(L,L);
+% %     h1(3:5,3:5) = 8;
+% %     
+% %     h2 = ones(L,L)/(L^2);    
+% %     h3 = conv2(h1,h2);    
+% 
+%     L = 3;
+%     h1 = -1*ones(L,L);
+%     h1(2,2) = 8;
+%     
+%     h2 = ones(L,L)/(L^2);    
+%     h3 = conv2(h1,h2);    
+% 
+%     [X,Y] = meshgrid(1:2*L-1 , 1:2*L-1);
+%     surf(fittedmodel(X,Y),h3)
+%     
+% %     L2 = 50;
+% %     x1 = ceil( (L2-(2*L-1))/2 );
+% %     H = zeros(L2,L2);
+% %     H(x1:x1+2*L-2,x1:x1+2*L-2) = h3;
+% % %     [X,Y] = meshgrid(1:L2 , 1:L2);
+% % %     surf(fittedmodel(X,Y),H)
+% % 
+% %     [X,Y] = meshgrid(1:2*L-1 , 1:2*L-1);
+% %     surf(fittedmodel(X,Y),h3)
+
+%%
+% app_designer_address: 'C:\Users\artin\AppData\Roaming\MathWorks\MATLAB Add-Ons\Collections\App Designer - Image Processing'
